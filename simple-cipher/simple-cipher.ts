8fc91fa5ab55fd9896080ed760496fe7b5017c1f
jeswinsimon <jeswinsimon@gmail.com>
2020-01-07 07:15:08 +0530

    Updated simple cipher solution based on mentor suggestions

--- simple-cipher/simple-cipher.ts
+++ simple-cipher/simple-cipher.ts
@@ -1,54 +1,54 @@
 class SimpleCipher {
-    key: string;
+    readonly key: string
+    private static charCodeLowercaseA = 'a'.charCodeAt(0)
     constructor(keyString = SimpleCipher.generateKey()) {
-        this.key = SimpleCipher.validateKey(keyString);
+        this.key = SimpleCipher.validateKey(keyString)
     }
 
     encode(plainText: string): string {
-        const mKey = SimpleCipher.getKeyForMessage(plainText, this.key);
-        let encryptedArray = [...plainText].map((char, index) =>
-            SimpleCipher.getCharFromCode((char.charCodeAt(0) - 97) + (mKey.charCodeAt(index) - 97)));
-        return encryptedArray.join('');
+        const mKey = SimpleCipher.getKeyForMessage(plainText, this.key)
+        const encryptedArray = [...plainText].map((char, index) =>
+            SimpleCipher.getCharFromCode((char.charCodeAt(0) - SimpleCipher.charCodeLowercaseA) + (mKey.charCodeAt(index) - SimpleCipher.charCodeLowercaseA)))
+        return encryptedArray.join('')
     }
 
     decode(encryptedMessage: string): string {
-        const mKey = SimpleCipher.getKeyForMessage(encryptedMessage, this.key);
-        let decodedArray = [...encryptedMessage].map((char, index) =>
-            SimpleCipher.getCharFromCode((char.charCodeAt(0) - 97) - (mKey.charCodeAt(index) - 97)));
-        return decodedArray.join('');
+        const mKey = SimpleCipher.getKeyForMessage(encryptedMessage, this.key)
+        const decodedArray = [...encryptedMessage].map((char, index) =>
+            SimpleCipher.getCharFromCode((char.charCodeAt(0) - SimpleCipher.charCodeLowercaseA) - (mKey.charCodeAt(index) - SimpleCipher.charCodeLowercaseA)))
+        return decodedArray.join('')
     }
-
 
     /* Generate a lowercase alphabetical key of length 100 */
     private static generateKey(): string {
-        return [...Array(100)].map(this.getRandomKeyChar).join('');
+        return [...Array(100)].map(this.getRandomKeyChar).join('')
     }
 
     /* Generate a random lowercase alphabet */
     private static getRandomKeyChar(): string {
-        return String.fromCharCode(97 + Math.floor(Math.random() * 26));
+        return String.fromCharCode(SimpleCipher.charCodeLowercaseA + Math.floor(Math.random() * 26))
     }
 
     private static validateKey(keyString: string): string {
         if (!keyString.match(/^[a-z]+$/)) {
-            throw new Error('Bad key');
+            throw new Error('Bad key')
         }
-        return keyString;
+        return keyString
     }
 
     private static getCharFromCode(charCode: number): string {
-        charCode = charCode % 26;
-        charCode = charCode < 0 ? charCode + 26 : charCode;
-        return String.fromCharCode(charCode + 97);
+        charCode = charCode % 26
+        charCode = charCode < 0 ? charCode + 26 : charCode
+        return String.fromCharCode(charCode + SimpleCipher.charCodeLowercaseA)
     }
 
     /* Augment key length to be greater than message length */
     private static getKeyForMessage(message: string, key: string): string {
-        let mKey = key;
+        let mKey = key
         while (message.length > mKey.length) {
-            mKey += mKey;
+            mKey += mKey
         }
-        return mKey;
+        return mKey
     }
 }
 
